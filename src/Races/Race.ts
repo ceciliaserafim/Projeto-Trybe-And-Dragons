b18b7940698e5e69abb5eb608f047449b60e49e6
Cecília Serafim <ceciliaserafimti@gmail.com>
2023-02-23 14:42:55 -0300

    Adiciona início do requisito 1

---
+++ src/Races/Race.ts
@@ -0,0 +1,13 @@
+abstract class Race {
+  private name: string;
+  private dexterity: number;
+
+constructor(name: string, dexterity: number;) {
+  this.name = name,
+  this.dexterity = dexterity
+  }
+
+  createdRacesInstances
+}
+
+export default Race;
