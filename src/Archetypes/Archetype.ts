5c3f28238ff77f618206dcf50251800b3b6106eb
Cecília Serafim <ceciliaserafimti@gmail.com>
2023-02-24 21:09:58 -0300

    Adiciona requisito 4, precisando de ajustes

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,33 @@
+type EnergyType = 'mana' | 'stamina';
+
+abstract class Archetype {
+  private _name: string; 
+  private _special: number; 
+  private _cost: number;
+
+  constructor(name: string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  get name(): string {
+    return this.name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  public static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  public abstract energyType(): EnergyType;
+}
+
+export default Archetype;
